f833a073d6167d9a620d09c750b54cae93805851
arpantyagi <arpantyagiat@outlook.com>
2022-01-20 19:53:42 +0100

    Working v1

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# Text-Adventure-Game
-A text based adventure inspired from "Zork" that was popular in the 80s.
+# Text Adventure in Prolog
+
+[Here is the Link to the Task on Course Page ](https://courses.fit.cvut.cz/BIE-PPA/projects/adventure.html)
+
+### Here is highlight of what is required from the solution 
+
+- [x] Text Based adventure games similar to the ones popular in 80s
+- [x] Describe a way in which a dungeon can be encoded.
+- [x] 20 room with Textual description
+- [x] path between the rooms
+- [x] must inform the user when objective is reached
+- [x] define functions for movement ( The course page asked us to define functions for north, south, east, west movement. I tried to be more creative and at every step I inform the player what rooms he can go to and then he just needs to type the name of the room he wants to go in. My reasoning being that this is more natural way of navigation a dungeons filled with many rooms.)
+- [x] add a player inventory, where the player can store items he picks up during the game
+- [x] Locked Door that can be open if the user posses the specific key in their inventory
+- [x] The door remains open once unlocked by the key
+- [x] once player unlocks the door the player looses the key
+
+### Extra Points 
+
+- [x] Two Types of Weapon
+- [x] Two Types of Enemies
+- [x] Each Enemy immune to one of the weapons ( There are two types of enemies Guard and Knight, Knight is immune to knife but can be killed by sword, and vice versa for guard.)
+- [ ] Player Lives
+- [x] Killing Enemy if proper weapons possesed in inventory.
+- [ ] Extra lives to be collected from some room.( There are food items distributed all over the rooms, but Unfortunately I coudn't implement multiple lives part.)
+- [x] Ability to Teleport if player holds the device in inventory
+- [x] define function Use for it.
+ 
+As this project was marked with !! sign and I have tried my level best to implement all the additional features just partially missing two of them. I am expecting 28-30 points. 
+If there is some scope of improvement(obviously there is) which can help me get more points, please let me know I am willing to spend a few more hours, polishing the project to get more points.
+
+
+## Game Information
+
+- Please have look at the above section to see all the required and additional features fullfilled by the game.
+- At the bottom there will be screenshots of example run of the game from start to finish.
+
+### Dungeon Prison Escape.
+
+- This game is inspired by 80s style text adventure game Zork. The player has just discovered that his prison cell's lock has been opened and ther is no guard around.The Player's goal is to escape the Dungeon.
+- He must collect neccesary keys that will give him access to rooms that contain weapons which are required to defeat the Knight"s who is guarding the exit of the dungeon. There are sufficient hint provided during the game,
+
+### Extra Notes
+
+- I have tried to cover all edge cases, entering room without keys, travelling to rooom not directly connected to present room, travelling to the same room, going to enemy without any weapons.
+- But if still there are some edge cases that remain and I can get significant points if I do them please let me know, I would be happy to work on them.
+
+## Example usage
+
+The First two steps are obvious launching the prolog listner and then consulting my file, using the following command.
+`swipl`
+
+`consult(tyagiarp)`
+
+In the devlop branch you will find folder game_run which contains screenshots of the game run from start to finish.
+-
--- tyagiarp.pl
+++ tyagiarp.pl
@@ -7,13 +7,9 @@
 :- dynamic health/1.
 :- dynamic status/1.
 :- dynamic player/1.
-
+:- dynamic enemy/3.
 :- ansi_format([bold,fg(green)],"Enter 'go.' to begin the 'Dungeon Escape'.", []).
 
-
-/*------------------Game Facts------------------*/
-here('Knight"s Room').
-player('alive').
 
 
 /*------------------Game Shortcuts ------------------*/
@@ -36,24 +32,25 @@ player('alive').
 'Outside' :- goto('Outside').
 
 /*------------------Game Start------------------*/
-
+here('Prison Cell').
+player('alive').
 go :- welcome(), nl, look.	
-	here('Knight"s Room').
-	player('alive').
+	
 
 
 welcome() :- 
 			ansi_format([fg(red)],"Dungeon Prison Escape", []), nl,nl,
 			ansi_format([fg(cyan)],  "This game was developed as a project for the BIE-PPA course at Czech Technical University FIT. Please have a look at the Readme.md for more info.", []),nl,nl,
-			ansi_format([fg(blue)],  "Your primary objective is escape the Dungeon, You will need to collect specific weapons to defeat specific enemies, you will need to eat edible food to restore Health if you are attacked, but be careful to avoid poisonous food. All the Best.", []),nl,
-
+			ansi_format([fg(blue)],  "Your primary objective is to escape the Dungeon, You will need to avoid enemies and entering the same room as an enemy without the proper weapon will lead to DEATH, Make sure to scout the area and look for weapons before you go behind any suspicious doors.", []),nl,
+			ansi_format([fg(blue)],  "Whenever you enter any room, You will receive information about the next rooms you can go to as well as the items in the room you can pick up, write your choice inside single apostrophe ('). For example if you want to go to Upper Dungeons you need to write 'Upper Dungeons' to the listner with the single apostrophe (').", []),nl,
 			write("go.            -->     Begin the Escape"), 					nl,
 			write("inventory.     -->     Check your Inventory"), 				nl,
 			write("look.          -->     look around your environment"), 	nl,
-			write("take(object).  -->     Stores object in your inventory"), 	nl,
-			write("drop(object).  -->     Drops object from your inventory"), 	nl,
-			write("restart.       -->     Restarts the game"), 					nl,
-			nl, nl.
+			write("take('object').  -->     Stores object in your inventory"), 	nl,
+			write("drop('object').  -->     Drops object from your inventory"), 	nl,
+			write("use.           -->     use the teleport."), 					nl,
+			write("halt.          -->     Exit the game"), 					nl,
+			nl, nl, !.
 
 /*------------------Available Rooms------------------*/
 room('Prison Cell').
@@ -76,24 +73,22 @@ room('Outside').
 
 /*------------------Room Descriptions ------------------*/
 
-description('Prison Cell', 'Its Dark, Damp and Cold in the Cell, There seems nothing of Interest here! You must Escape the Dungeon Prison').
-
-description('Main Prison Corridor', 'You have stepped out of your "Prison Cell" and into a long corridor, which has prison cells on both sides. Most of them are empty and contain skeleton of people who have died after spending their lives in this dungeon. You must not suffer the same fate. Its a very hard to see in the dark Corridor, the only source of light is the wooden torch on the wall, However you can see an "Unlocked Prison Cell" on the right. There is also a narrow stairway going to the "Upper Dungeons".').
-description('Unlocked Prison Cell','Looks Like someone was recently here, There are some "Apple" here that you might be able to eat. Be careful what you eat though! Eating the wrong things can decrease your Health!').
-
-description('Locked Prison Cell','').
-description('Key Room', '').
-description('Upper Dungeons','You enter a big Hall, It is dimly lit, The only source of light is the Fireplace adjacent to the stairway, there is a big Dining Table in the Center, There is some Rotting Meat on the Table. There is a Heavy Iron door on the front which leads to the "Guard Room", you can hear some footsteps and metal armour clanking. On the right there is a door that leads to the "Long Hall", you can hear someone talking there. On the left there is a wooden door which leads to the "Passageway".", ').
-description('Long Hall','It is very Dark, you can only see your surroundings thanks to the wooden torch, The walls are covered with portraits of the Devil and deomons of different kind, the floor is covered in dried blood, On the other end of there is a door that leads to the "Execution Room".').
-description('Execution Chamber','').
-description('Passageway','').
-description('Armoury','').
-description('Kitchen','').
-description('Aragog"s Lair','').
-description('Knight"s Room','').
-description('Tall Stairway','').
-description('Tunnel','').
-description('Outside','Success').
+description('Prison Cell', 'Its Dark, Damp and Cold in the Cell, There seems nothing of Interest in here! You must quickly escape the Dungeon Prison, the lock to the prison was rusty and has been broken, its a good time to escape to the "Main Prison Corridor".').
+description('Main Prison Corridor', 'You have stepped out of your "Prison Cell" into a long corridor, which has prison cells on both sides. Most of them are empty and contain skeleton of people who have died after spending their lives in this dungeon. You must not suffer the same fate. Its a very hard to see in the dark Corridor, the only source of light is the wooden torch on the wall, However you can see an "Unlocked Prison Cell" on the right and there is "Locked Prison Cell" to the left you can see a note of some sort there on the floor, it might be helpful to find the key and read the note.There is also a narrow stairway going to the "Upper Dungeons".').
+description('Unlocked Prison Cell','Looks Like someone was recently here, There are some "Apple" here that you might be able to eat. There is a message on the wall inscribed with fingernails it reads "Get the Knife". Hmm.... Intersting where would you be able to find a Kniife here. ').
+description('Locked Prison Cell','The Prison is not much differnt from yours but you can see a MAP here! The map details the way to exit the Prison. Here is what it says "Fight The Knight in Knight"s Room, Take the Stairs towards the Tunnel and soon you will see Sunshine.').
+description('Key Room', 'The room is filled with portraits, and Keys, However one key stands out from the rest, the shining silver key, it is lablled to the "Armoury", I am sure you can find some weapons there, There are no doors leading elsewhere This is a dead end, You can only go back from here.').
+description('Upper Dungeons','You enter a big Hall, It is dimly lit, The only source of light is the Fireplace adjacent to the stairway, there is a big Dining Table in the Center, There is some Rotting Meat on the Table. There is a Heavy Iron door on the front which leads to the "Knight"s Room", you can hear some footsteps and metal armour clanking. On the right there is a door that leads to the "Long Hall". On the left there is a wooden door which leads to the "Passageway".').
+description('Long Hall','It is very Dark, you can only see your surroundings thanks to the wooden torch, The walls are covered with portraits of the Devil and deomons of different kind, the floor is covered in dried blood, On the other end of there is a door that leads to the "Execution Chamber", you can hear someone inside the room, you better have a weapon before venturing there.').
+description('Execution Chamber','You are shocked, you see several decapitated  human body parts, for the first time you witness the guillotine. This seems a bad room to be in.').
+description('Passageway','You enter the slim corridor which has doors in all direction, but you can see something shining very brightly in here, It is the teleport!!, you can pick it up and get back to this location using it. You can also notice a door to the "Kitchen" on the right, a heavy metal door to the "Armoury" on the left and a very weird looking black door that is lablled "Aragog"s Lair".').
+description('Armoury','This room is well lit and here are weapons and Armours of all kind you should definetly take some weapons and fight the guard in Knight"s Room. There is also a key to the Locked Prison here, Do you think it is worth it to travel back to the Locked Prison ?').
+description('Kitchen','Standard Dark Kitchen, There is no one here, However a Knife might be handy weapon to take.').
+description('Aragog"s Lair','You should get out of this room!, there is nothing of Interest and there are spiders all over the room, RUN!').
+description('Knight"s Room','This Room is Huge and there are heavy chandeliers, the room is lit with candles all over the place, there is a reading table here, seems like someone was recently here, There is a Knight standing in the corner.').
+description('Tall Stairway','You have come across a very steep and long stairway for as far as you see you can just see stairs and both side are walls which are splattered with dried blood, You should keep going straight ahead. There seems to be Tunnel that might lead to the outside.').
+description('Tunnel','You have stepped inside the tunnel and you can already see some sunlight, its the first time in months you are seeing sunlight, just a few more steps to the outside.').
+description('Outside','Congratulations, Success! you have completed the Game Dugneon Prison Escape.').
 
 
 
@@ -154,8 +149,9 @@ edible('Cold Soup').
 inedible('Rotten Meat').
 
 /*------------------ Enemies and location ------------------*/
-enemy('Guard', 'Execution Chamber', 'Long Hall').
+
 enemy('Knight', 'Knight"s Room' , 'Upper Dungeons').
+enemy('Guard', 'Execution Chamber', 'Long Hall').
 
 /*------------------List all the Things in the Place------------------*/
 
@@ -225,11 +221,11 @@ move(Place) :-
 
 game_status() :-
 	here('Outside'),
-  	ansi_format([bold,fg(white),bg(yellow)], 'VICTORY you have escaped the Prison!', []), nl,
-  	false.
+  	ansi_format([bold,fg(green),bg(yellow)], 'VICTORY you have escaped the Prison!', []), nl,
+  	halt.
 game_status() :-
 	player('dead'),
-  	ansi_format([bold,fg(white),bg(yellow)], 'Defeat you Died!', []), nl,
+  	ansi_format([bold,fg(red),bg(yellow)], 'Defeat you Died!', []), nl,
   	false.
   	game_status() :- 
 	true.
@@ -237,29 +233,31 @@ game_status() :-
 enemies(Place) :- 
 	here(X),
 	enemy('Knight', Place, X ),
-	have('Iron Sword'), 
+	have('Steel Sword'), 
 	retract(enemy('Knight', Place, X)),
-	retract(have('Iron Sword')),
-	write('Thankfully, you had the Iron Sword in your Inventory after an intense battle you were Luckliy able to beat the Knight.'), nl, nl, !.
+	retract(have('Steel Sword')),
+	write('As you opne the door you are greeted by a swift blade to your chest, duck and roll under the Knight Thankfully, you had the Steel Sword in your Inventory after an intense battle you were luckliy able to beat the Knight.'), nl, nl.
 enemies(Place) :- 
 	here(X),
 	enemy('Knight', Place, X ),
 	retract(player('alive')),
 	asserta(player('dead')),
-	write('You barge into the Knight"s Room unprepared and you pay the price, the immediately attacks you and you are dead.'),nl,nl,!, 
+	write('You barge into the Knight"s Room unprepared and you pay the price, he immediately attacks you and you have no chances to fight against well trained armed Knight. You are dead.'),nl,nl, 
+	write('GAME OVER'),
 	halt.
 enemies(Place) :- 
 	here(X),
 	enemy('Guard', Place, X ),
-	have('Kitchen Knife'), 
+	have('Knife'), 
 	retract(enemy('Guard', Place, X)),
-	write('Thankfully, the Guard was unarmed and you were able to kill him with the Kitchen Knife, you picked up earlier.'), nl, nl, !.
+	write('You have been spotted by a guard, the guard looks angry but he seems to be unarmed.After an intense battle you were able to stab him multiple times and you were able to kill him with the Kitchen Knife, you picked up earlier.'), nl, nl.
 enemies(Place) :- 
 	here(X),
 	enemy('Guard', Place, X ),
 	retract(player('alive')),
 	asserta(player('dead')),
-	write('You go to the execution room and are face to face with a guard after an intense battle he pins you to the ground and beats you to death.'),nl,nl,!,
+	write('You go to the execution room and are face to face with a guard after an intense battle he pins you to the ground and beats you to death. You should have got a Weapon to a fight !'),nl,nl,
+	write('GAME OVER'),
 	halt.
 enemies(_).
 
@@ -286,21 +284,22 @@ special(_).
 
 use :- 
 	have('teleport'),
-	retract(here(X)),
+	retract(here(_)),
 	retract(have('teleport')),
 	asserta(location('teleport', 'Passageway')),
 	asserta(here('Passageway')),
-	write('You have been magically teleported back to the Passageway.').
+	write('You have been magically teleported back to the Passageway.'),
 	look.
-have('teleport').
+use :- 
+	write('You do not have the teleport in inventory.').
 
 /*------------------Enemies------------------*/
 
 /*------------------Taking Objects------------------*/
 
 take(X) :-
-	can_take(X),
-	take_object(X),
+	can_take(X),!,
+	take_object(X),!,
 	true.
 
 can_take(Thing) :- 
@@ -308,13 +307,13 @@ can_take(Thing) :-
 	location(Thing, Place),!.
 can_take(Thing) :- 
 	write('There is no '), write(Thing), write(' at this location.'),
-	nl, fail.
+	nl.
 
 take_object(Thing) :- 
 	here(X),
 	retract(location(Thing,X)),
 	asserta(have(Thing)),
-	write(Thing), write(' added to Inventory.'),
+	write(Thing), write(' added to Inventory.'),!,
 	nl.
 
 /*------------------Dropping Objects------------------*/
